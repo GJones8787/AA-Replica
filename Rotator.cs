28f91aef396caad2ae4e3b713a6d35ac3a43340a
GJones8787 <71368383+GJones8787@users.noreply.github.com>
2021-03-12 20:48:12 -0600

    AA-Replica

---
+++ Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed = 100f;
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, speed * Time.deltaTime);
+    }
+
+    
+}
